c95a8dceed0ce2c55e76de4a1119d3fcd32fc5bf
openPyramid <openPyramid@163.com>
2019-09-16 18:29:03 +0800

    Learn constrain\chain; and set listener to change background color when view is clicked.

--- app/src/main/java/com/example/colormyviews/MainActivity.kt
+++ app/src/main/java/com/example/colormyviews/MainActivity.kt
@@ -1,12 +1,49 @@
 package com.example.colormyviews
 
+import android.annotation.SuppressLint
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setListeners()
+    }
+
+    private fun setListeners() {
+        val clickableViews: List<View> =
+            listOf(box_one_text, box_two_text, box_three_text, box_four_text, box_five_text,ConstraintLayout)
+
+        for (item in clickableViews) {
+            item.setOnClickListener { makeColored(it)}
+        }
+    }
+
+    @SuppressLint("ResourceAsColor")
+    private fun makeColored(view: View) = when (view.id) {
+        R.id.box_one_text -> {
+            view.setBackgroundColor(Color.BLUE)
+        }
+        R.id.box_two_text -> {
+            view.setBackgroundColor(Color.RED)
+        }
+        R.id.box_three_text -> {
+            view.setBackgroundColor(Color.BLACK)
+        }
+        R.id.box_four_text -> {
+            view.setBackgroundColor(Color.BLUE)
+        }
+        R.id.box_five_text -> {
+            view.setBackgroundColor(Color.DKGRAY)
+        }
+
+        else -> {
+            view.setBackgroundColor(Color.YELLOW)
+        }
     }
 }
